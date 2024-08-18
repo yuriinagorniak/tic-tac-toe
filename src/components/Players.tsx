0fc067deff43d4acc144d6500527a7700d929685
thisisnago <yurii.nagorniak@gmail.com>
2024-08-18 17:17:44 +0100

    feat: add handleNameChange to save new player's name

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
-export const Player = (): JSX.Element => {
+interface PlayerProps {
+    name: string;
+}
+
+export const Player = ({ name }: PlayerProps): JSX.Element => {
     const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [currentName, setCurrentName] = useState<string>(name);
 
     const handleEditClick = () => {
-        
+        setIsEditing((prev) => !prev);
+    };
+
+    const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.value) {
+            setCurrentName(event.target.value);
+        }
     }
 
     return (
-        <li>
-            <p className="player-name">Player1</p>
+        <li className="player">
+            {isEditing ? <input required type="text" value={currentName} onChange={handleNameChange}/> : <p className="player-name">{currentName}</p>}
             <p className="player-symbol">X</p>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 };
